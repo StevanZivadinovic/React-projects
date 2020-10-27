5c52153c255c996917131b602ed09e0ccdf2dcbd
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-10-27 16:22:52 +0100

    git commit -m""

--- rent-a-car/rent-a-car/src/App.js
+++ rent-a-car/rent-a-car/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import Komponenta from "./components/komponenta";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-
-
- 
   return (
     <div className="App">
       <header className="App-header">
-      
+        <Router>
+          <Navbar></Navbar>
+
+          {/* <Route path="/" exact component={Home}></Route>
+          <Route path="/services" exact component={Services}></Route>
+          <Route path="/products" exact component={Products}></Route>
+          <Route path="/sign-up" exact component={SignUp}></Route> */}
+        </Router>
       </header>
     </div>
   );
