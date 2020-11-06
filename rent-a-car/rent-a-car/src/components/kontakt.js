8d6e7fb69e8af9c6e80d5ef79a2b6da25fc97652
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-11-06 21:08:46 +0100

    git commit -m""

--- rent-a-car/rent-a-car/src/components/kontakt.js
+++ rent-a-car/rent-a-car/src/components/kontakt.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 // import Footer from "./footer";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import fb from "./facebook.svg";
-import './kontakt.css';
+import "./kontakt.css";
 
 export default function Kontakt() {
+  let a = document.getElementById('porukaInput');
+
+  function setCaretPosition(ctrl, pos) {
+    // For Most Web Browsers
+    if (ctrl.setSelectionRange) {
+      ctrl.focus();
+      ctrl.setSelectionRange(pos, pos);
+    // IE8 and below
+    } 
+  }
+
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const porukaRef = useRef();
+  const submitRef = useRef();
+
+  useEffect(() => {
+    nameRef.current.focus();
+  }, []);
+
+  function keyPressHandle(e) {
+    if (e.keyCode === 13) {
+      if (e.target.id === "nameInput") {
+        emailRef.current.focus();
+      }
+      if (e.target.id === "emailInput") {
+        porukaRef.current.focus();
+        let a = document.querySelector('#porukaInput');
+        
+      }
+      
+    }
+  }
+
+  function onClickHandle() {
+    alert("submitted");
+  }
+
   return (
     <div className="main3">
       <div className="contactDetails">
@@ -20,19 +59,41 @@ export default function Kontakt() {
         </p>
       </div>
       <div className="contactForm">
-        <form>
-          
-          <div><span>Име</span><input type="text"></input>
-          </div>
-          <div><span>Име</span><input type="text"></input>
-          </div>
-          <div><span>Име</span><input type="text"></input>
-          </div>
-
-          
-          
-          
-        </form>
+        <div className="form-field">
+          <span>Име</span>
+          <input
+            ref={nameRef}
+            id="nameInput"
+            type="text"
+            onKeyDown={keyPressHandle}
+          />
+        </div>
+        <div className="form-field">
+          <span>Email</span>
+          <input
+            ref={emailRef}
+            id="emailInput"
+            type="email"
+            onKeyDown={keyPressHandle}
+          />
+        </div>
+        <div className="form-field">
+          <span>Порука</span>
+          <textarea
+            ref={porukaRef}
+            id="porukaInput"
+            onKeyDown={keyPressHandle}
+            onClick={setCaretPosition}
+          />
+        </div>
+        <button
+          onClick={onClickHandle}
+          ref={submitRef}
+          id="submitButton"
+          onKeyDown={keyPressHandle}
+        >
+          Submit
+        </button>
       </div>
 
       {/* <div className="footer" id='footer1'>
