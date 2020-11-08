20ad6b50f8d05a94732736a75ca564c91c0de9a2
StevanZivadinovic <stevanzivadinovic@outlook.com>
2020-11-08 15:54:16 +0100

    git commit -m""

--- rent-a-car/rent-a-car/src/components/kontakt.js
+++ rent-a-car/rent-a-car/src/components/kontakt.js
@@ -5,40 +5,39 @@ import fb from "./facebook.svg";
 import "./kontakt.css";
 
 export default function Kontakt() {
-  let a = document.getElementById('porukaInput');
+  let a = document.getElementById("porukaInput");
 
   function setCaretPosition(ctrl, pos) {
     // For Most Web Browsers
     if (ctrl.setSelectionRange) {
       ctrl.focus();
       ctrl.setSelectionRange(pos, pos);
-    // IE8 and below
-    } 
+      // IE8 and below
+    }
   }
-
 
   const nameRef = useRef();
   const emailRef = useRef();
   const porukaRef = useRef();
   const submitRef = useRef();
 
-  useEffect(() => {
-    nameRef.current.focus();
-  }, []);
+  // useEffect(() => {
+  //   nameRef.current.focus();
+  // }, []);
 
-  function keyPressHandle(e) {
-    if (e.keyCode === 13) {
-      if (e.target.id === "nameInput") {
-        emailRef.current.focus();
-      }
-      if (e.target.id === "emailInput") {
-        porukaRef.current.focus();
-        let a = document.querySelector('#porukaInput');
-        
-      }
-      
-    }
-  }
+  // function keyPressHandle(e) {
+  //   if (e.keyCode === 13) {
+  //     if (e.target.id === "nameInput") {
+  //       emailRef.current.focus();
+  //     }
+  //     if (e.target.id === "emailInput") {
+  //       porukaRef.current.focus();
+  //       let a = document.querySelector('#porukaInput');
+
+  //     }
+
+  //   }
+  // }
 
   function onClickHandle() {
     alert("submitted");
@@ -46,61 +45,39 @@ export default function Kontakt() {
 
   return (
     <div className="main3">
-      <div className="contactDetails">
-        <p>stevanzivadinovic@outlook.com</p>
-        <p>061/321-60-87</p>
-        <p>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/Pravoslavne-ikone-Nis-1691948927791453"
-          >
-            <img id="fb" src={fb}></img>
-          </a>
-        </p>
+      <div className="wrapper">
+        <div className="title">
+          <h1>контакт форма</h1>
+        </div>
+        <div className="contact-form">
+          <div className="input-fields">
+            <input type="text" class="input" placeholder="Name"></input>
+            <input
+              type="text"
+              class="input"
+              placeholder="Email Address"
+            ></input>
+            <input type="text" class="input" placeholder="Phone"></input>
+            <input type="text" class="input" placeholder="Subject"></input>
+          </div>
+          <div className="msg">
+            <textarea placeholder="Message"></textarea>
+            <div className="btn">send</div>
+          </div>
+        </div>
+        <div className="contactDetails">
+          <p>stevanzivadinovic@outlook.com</p>
+          <p>061/321-60-87</p>
+          <p>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/Pravoslavne-ikone-Nis-1691948927791453"
+            >
+              <img id="fb" src={fb}></img>
+            </a>
+          </p>
+        </div>
       </div>
-      <div className="contactForm">
-        <div className="form-field">
-          <span>Име</span>
-          <input
-            ref={nameRef}
-            id="nameInput"
-            type="text"
-            onKeyDown={keyPressHandle}
-          />
-        </div>
-        <div className="form-field">
-          <span>Email</span>
-          <input
-            ref={emailRef}
-            id="emailInput"
-            type="email"
-            onKeyDown={keyPressHandle}
-          />
-        </div>
-        <div className="form-field">
-          <span>Порука</span>
-          <textarea
-            ref={porukaRef}
-            id="porukaInput"
-            onKeyDown={keyPressHandle}
-            onClick={setCaretPosition}
-          />
-        </div>
-        <button
-          onClick={onClickHandle}
-          ref={submitRef}
-          id="submitButton"
-          onKeyDown={keyPressHandle}
-        >
-          Submit
-        </button>
-      </div>
-
-      {/* <div className="footer" id='footer1'>
-        <Router>
-          <Footer></Footer>
-        </Router>
-      </div> */}
     </div>
   );
 }
