32edc383669de25c49f332ed8389fb757ef89c86
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-01-06 00:23:21 +0100

    git commit -m""

--- dodjo-app/src/components/home.js
+++ dodjo-app/src/components/home.js
@@ -1,9 +1,30 @@
 //sfc - statles functional component
+import { useState } from "react";
 
 const Home = () => {
-    return ( <div className="content">
-        <h1>Home page</h1>
-    </div> );
-}
- 
+  const [blogs, setBlogs] = useState([
+    {
+      title:'ozonizacija',  
+      ime: "stefana",
+      prezime: "podolski",
+      id: 1,
+    },
+    {title:'pesticidi', ime: "milijana", prezime: "djokic", id: 2 },
+  ]);
+
+  return (
+    <div className="content">
+      <h1>Home page</h1>
+      <p>pozz</p>
+      <div>{blogs.map(blog=>{
+          return ( 
+          <div key={blog.id}>
+            <h1>{blog.title}</h1>
+            <p>{blog.ime}</p>
+          </div>)
+      })}</div>
+    </div>
+  );
+};
+
 export default Home;
