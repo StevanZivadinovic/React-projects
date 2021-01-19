8c4665462d2c3a117a7389323c596cb753ed484f
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-01-19 18:12:07 +0100

    git commit -m""

--- video-search/src/App.js
+++ video-search/src/App.js
@@ -1,37 +1,42 @@
 //instalacija npm install --save faker
-import faker from "faker";
-import { useState } from "react";
-import Avatar from "./components/season";
+// import faker from "faker";
+import { useState, useEffect } from "react";
 import "./style/style.css";
+import Season from './components/season.js';
 
 function App() {
-// brisanje npm paketa iz projekta: npm uninstall --save ime projekta-npm, 
+  // brisanje npm paketa iz projekta: npm uninstall --save ime projekta-npm,
 
-const [lat, setLat]=useState(null);
-const [long, setLong]=useState(null);
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+useEffect(()=>{
+  window.navigator.geolocation.getCurrentPosition(
+    (response) => {
+      setLat(response.coords.latitude);
+      setLong(response.coords.longitude);
+      console.log(response.coords.latitude);
+    },
+    (err) => {
+      console.log(err.message);
+    }
+  );
 
-
-
-window.navigator.geolocation.getCurrentPosition(
-  (response)=>{setLat(response.coords.latitude)
-    setLong(response.coords.longitude)
-    console.log(response.coords.latitude)}
-    ,
-    err=>{console.log(err.message)}
-)
-
-    
-
+},[])
 
   return (
     <div className="App">
-      <Avatar></Avatar>
-    
-    {(lat && long)? <div className="ispis">{`lat:${lat}, long: ${long}`}
-    </div>:<div>Loading....</div>}
+     
+
+      {lat && long ? (
+        
+        <div className="ispis">{`lat:${lat}, long: ${long}`}
+        <Season lat={lat}></Season></div>
+        
+        
+      ) : (
+        <div>Loading....</div>
+      )}
     </div>
-
-
   );
 }
 
--- video-search/src/components/season.js
+++ video-search/src/components/season.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+// npm install --save react-fontawesome
+//semanticUI
+import Image from "./../components/snowflake";
+import Sun from "./../components/sun";
 
-const Avatar = ({names}) => {
-    return (
 
-         
-                <div className='polje'></div>);
-}
- 
-export default Avatar;
+const Season = ({ lat }) => {
+  const [mesec, setmesec] = useState(new Date().getMonth());
+  const [sezona, setSezona] = useState(null);
+  let sezona1;
+  if (mesec > 2 && mesec < 9) {
+    lat > 0 ? (sezona1 = "summer") : (sezona1 = "winter");
+    console.log("lat");
+  } else {
+    lat > 0 ? (sezona1 = "winter") : (sezona1 = "summer");
+    console.log("-lat");
+  }
+
+  return (
+    <div className="polje">
+      <div className="text">{sezona1}</div>
+      <div className="slika">{sezona1==='winter'?<Image></Image> : <Sun></Sun>}
+       
+       
+      </div>
+    </div>
+  );
+};
+
+export default Season;
