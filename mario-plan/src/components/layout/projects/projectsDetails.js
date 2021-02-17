6415f9f2b9f5116e0db0a677ae58f6f0bdca897d
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-02-17 20:40:10 +0100

    izmeni

--- mario-plan/src/components/layout/projects/projectsDetails.js
+++ mario-plan/src/components/layout/projects/projectsDetails.js
@@ -3,36 +3,29 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 
-
-
-
 const ProjectsDetails = (props) => {
   let id = useParams();
-  let id1=id.id
- console.log(props.projects);
+  let id1 = id.id;
+  console.log(props.projects);
 
-  
-  console.log(id.id)
+  console.log(id.id); 
   return (
+    <div>
       <div>
-      
-         <div>
-         <div>
-           <div className="contentDetails">
-            {props.projects && <h2>Project Details -{`${props.projects.id1}`} </h2>}
-             <p></p>
-           </div>
-           <div className="actionCard">
-             <div>{`Posted by`}</div>
-             <div>2nd September, 2am</div>
-           </div>
-         </div>
-       </div>
-      
-      
+        <div>
+          <div className="contentDetails">
+            {props.projects && (
+              <h2>Project Details -{`${props.projects[id1].title}`} </h2>
+            )}
+            <p> {props.projects && ( `${props.projects[id1].content}` )}</p>
+          </div>
+          <div className="actionCard">
+            <div>{props.projects && ( `${props.projects[id1].authorFirstName}` )}</div>
+            <div>2nd September, 2am</div>
+          </div>
+        </div>
       </div>
-     
-    
+    </div>
   );
 };
 
@@ -46,7 +39,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect( [
-    { collection : "kolekcija" }
-])
+  firestoreConnect([{ collection: "kolekcija" }])
 )(ProjectsDetails);
