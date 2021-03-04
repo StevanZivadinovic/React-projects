2767a3beef5f838078b63f33d6c21ede3ab8244b
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-03-04 12:41:26 +0100

    izmena

--- pravoslavne-ikone/pravoslavne-ikone/src/components/navbar.js
+++ pravoslavne-ikone/pravoslavne-ikone/src/components/navbar.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect }  from "react";
-import { Link } from 'react-router-dom';
-import  Button  from './button';
-import './navbar.css'
-import krst from './1.png'
-// Za slike sa font awesoma, uzimas cdn kod sa njihovog sajta koji se nalazi na tvom profilu,
-//i kopiras ga u index.html, u public folderu!!!!!!!!!!!!!!!!!
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "./button";
+import "./navbar.css";
+import krst from "./1.png";
 
 export default function Navbar(props) {
   const [click, setClick] = useState(true);
   const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const closeMobileMenu = () => setClick(true);
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
@@ -25,67 +23,60 @@ export default function Navbar(props) {
     showButton();
   }, []);
 
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
   return (
     <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Православне иконе - Ниш
+          </Link>
+          <div>
+            <img id="krst" src={krst}></img>
+          </div>
 
-    <nav className='navbar'>
-      <div className='navbar-container'>
-     
-        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-          Православне иконе - Ниш 
-          
-        </Link>
-        <div>
-          <img id='krst'  src={krst}></img>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Почетна
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/ikonopisac"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                О иконописцу
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/galerija"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Галерија
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                to="/kontakt"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Контакт
+              </Link>
+            </li>
+          </ul>
         </div>
-         
-        
-        <div className='menu-icon' onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-          
-        </div>
-
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-              Почетна
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/ikonopisac'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              О иконописцу
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link
-              to='/galerija'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Галерија
-            </Link>
-          </li>
-
-          <li className='nav-item'>
-            <Link
-              to='/kontakt'
-              className='nav-links'
-              onClick={closeMobileMenu}
-            >
-              Контакт
-            </Link>
-          </li>
-          
-        </ul>
-     
-      </div>
-    </nav>
-  </>
+      </nav>
+    </>
   );
 }
