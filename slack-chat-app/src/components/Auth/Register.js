723cb814c722ed9278bf4449228fee5bacbf649d
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-03-13 19:28:06 +0100

    izmena

--- slack-chat-app/src/components/Auth/Register.js
+++ slack-chat-app/src/components/Auth/Register.js
@@ -1,12 +1,22 @@
 import puzzle from './../../img/puzzle.svg';
+// import username from './../../img/username.svg';
+
 
 
 const Register = () => {
-    return ( <div className='headerForm'>
-    
+    return ( <div >
+        <div className='headerForm'>
         <img src={puzzle} alt='puzzle' style={{width:100}}/>
         <h1>Register for DevChat</h1>
-        
+        </div>
+        <div className="forma">
+            <input id='username' type="text" className="username" placeholder='Username'/>
+            <input id='email' type="email" className="email" placeholder='EmailAddress'/>
+            <input id='password' type="password" className="password" placeholder='Password'/>
+            <input id='passwordRepeat' type="password" className="passwordRepeat" placeholder='PasswordRepeat'/>
+
+        <button>Submit</button>
+        </div>
     </div> );
 }
  
