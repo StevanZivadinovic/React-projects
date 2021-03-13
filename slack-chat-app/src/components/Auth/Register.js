7e5cfbb945063f9faaa06ceef1bf2e0b7e82370c
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-03-13 19:43:36 +0100

    izmena

--- slack-chat-app/src/components/Auth/Register.js
+++ slack-chat-app/src/components/Auth/Register.js
@@ -1,22 +1,25 @@
 import puzzle from './../../img/puzzle.svg';
-// import username from './../../img/username.svg';
-
+import {Link} from 'react-router-dom';
 
 
 const Register = () => {
+    let handleChange=()=>{
+        console.log('haj')
+    }
     return ( <div >
         <div className='headerForm'>
         <img src={puzzle} alt='puzzle' style={{width:100}}/>
         <h1>Register for DevChat</h1>
         </div>
         <div className="forma">
-            <input id='username' type="text" className="username" placeholder='Username'/>
-            <input id='email' type="email" className="email" placeholder='EmailAddress'/>
-            <input id='password' type="password" className="password" placeholder='Password'/>
-            <input id='passwordRepeat' type="password" className="passwordRepeat" placeholder='PasswordRepeat'/>
+            <input onChange={handleChange} id='username' type="text" className="username" placeholder='Username'/>
+            <input onChange={handleChange} id='email' type="email" className="email" placeholder='EmailAddress'/>
+            <input onChange={handleChange} id='password' type="password" className="password" placeholder='Password'/>
+            <input onChange={handleChange} id='passwordRepeat' type="password" className="passwordRepeat" placeholder='PasswordRepeat'/>
 
         <button>Submit</button>
         </div>
+        <div className="message">Alredy a user? <Link to='/login'>Login</Link></div>
     </div> );
 }
  
