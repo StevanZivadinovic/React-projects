beccf5356978e6fc32c4797b5c5c8c95394c9015
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-03-25 16:47:15 +0100

    izmena

--- slack-chat-app/src/App.js
+++ slack-chat-app/src/App.js
@@ -1,57 +1,57 @@
-
-import './style/App.css';
+import "./style/App.css";
 //moras da Router uvedes u komponenti koja je hijerarhijski iznad one komponente
 //u kojoj koristis useHistory ili npr useLocation, inace nece da rade.
 //U ovom konkretnom slucaju undex.js je hijerarhijski iznad App.js, pa zato samo tamo
 //uveo Router, a ovde ostalo(Switch i Route)
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import Firebase from './config';
-import {useEffect} from 'react';
-import { connect } from 'react-redux';
-import {SetUser} from './actions/index';
-import Spinner from './spinner';
-
-
-
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Firebase from "./config";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { SetUser } from "./actions/index";
+import Spinner from "./spinner";
+import ColorPanel from "./components/ColorPanel/ColorPanel";
+import SidePanel from "./components/SidePanel/SidePanel";
 
 function App(props) {
-  
   let history = useHistory();
 
   useEffect(() => {
-    Firebase.default.auth().onAuthStateChanged(user=>{
-      if(user){
-        console.log(user)
-        console.log(props.stateProperty.user.isLoading)
+    Firebase.default.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(props.stateProperty.user.isLoading);
         props.SetUser(user);
         history.push("/");
       }
-    })
-   
-    
-  },[])
+    });
+  }, []);
 
-
-  return  (props.stateProperty.user.isLoading ? <Spinner></Spinner> :
-
-    (<div className="App">
-     
-       <Switch>
-         <Route path='/login'><Login></Login></Route>
-         <Route path='/register'><Register></Register></Route>
-       </Switch>
-       
-     
-    </div>)
-  )
+  return props.stateProperty.user.isLoading ? (
+    <Spinner></Spinner>
+  ) : (
+    <div className="App">
+      <Switch>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/register">
+          <Register></Register>
+        </Route>
+      </Switch>
+      <div>
+      <ColorPanel></ColorPanel>
+      <SidePanel></SidePanel>
+      </div>
+      
+    </div>
+  );
 }
 
-let mapStateToProps = state=>({
-  stateProperty:state
-})
+let mapStateToProps = (state) => ({
+  stateProperty: state,
+});
 
-export default connect(mapStateToProps, {SetUser})(App);//kad ovako dodas funkciju
+export default connect(mapStateToProps, { SetUser })(App); //kad ovako dodas funkciju
 //stavljas uglaste zagrade.
---
+++ slack-chat-app/src/components/ColorPanel/ColorPanel.js
@@ -0,0 +1,7 @@
+const ColorPanel = () => {
+    return ( <div>
+        ColorPanel
+    </div> );
+}
+ 
+export default ColorPanel;
