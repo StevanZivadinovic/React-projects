4995346b0e8e9c3863121a7f69efe7264f3ffa58
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-03-25 20:11:38 +0100

    izmena

--- slack-chat-app/src/components/SidePanel/SidePanel.js
+++ slack-chat-app/src/components/SidePanel/SidePanel.js
@@ -2,6 +2,12 @@ import './../../style/App.css'
 const SidePanel = () => {
     return ( <div className='sidePanel'>
         <h1><span><img src="https://img.icons8.com/color/40/000000/slack-new.png"/></span>DevChat</h1>
+
+        <ul name="" id="">
+            <li value="" disabled>Signed in as User</li>
+            <li value="">Change avatar</li>
+            <li value="">Sign Out</li>
+        </ul>
     </div> );
 }
  
