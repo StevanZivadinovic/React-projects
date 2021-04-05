97b2b0c33d9a6a2fa73a61b353cf393cbd7ddfb8
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-04-05 21:25:12 +0200

    change

--- slack-chat-app/src/components/Messages/messageForm.js
+++ slack-chat-app/src/components/Messages/messageForm.js
@@ -2,10 +2,12 @@ const MessageForm = () => {
     return ( <div className="messageFormMain">
         <div className="inputText">
             <div>
-
             <span><img src="https://img.icons8.com/emoji/48/000000/plus-emoji.png"/><input type="text" placeholder='Write your message'/></span>
             </div>
         </div>
+            <div className='messageFormButton'>
+                <span><button className='addReplay'>Add Replay</button><button className='uploadMedia'>Upload Media</button></span>
+            </div>
     </div> );
 }
  
