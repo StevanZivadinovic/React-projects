365b36114533f8cc65993abe14d7415c3060fd5c
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-04-05 21:49:27 +0200

    change

--- slack-chat-app/src/components/Messages/messageForm.js
+++ slack-chat-app/src/components/Messages/messageForm.js
@@ -1,14 +1,24 @@
 const MessageForm = () => {
-    return ( <div className="messageFormMain">
-        <div className="inputText">
-            <div>
-            <span><img src="https://img.icons8.com/emoji/48/000000/plus-emoji.png"/><input type="text" placeholder='Write your message'/></span>
-            </div>
+  return (
+    <div className="messageFormMain">
+      <div className="inputText">
+        <div>
+          <span>
+            <img src="https://img.icons8.com/emoji/48/000000/plus-emoji.png" />
+            <input type="text" placeholder="Write your message" />
+          </span>
         </div>
-            <div className='messageFormButton'>
-                <span><button className='addReplay'>Add Replay</button><button className='uploadMedia'>Upload Media</button></span>
-            </div>
-    </div> );
-}
- 
+      </div>
+      <div className="messageFormButton">
+        <span>
+          <img src="https://img.icons8.com/fluent-systems-filled/48/000000/edit-message.png" />
+          <button className="addReplay">Add Replay</button>
+          <button className="uploadMedia">Upload Media</button>
+          <img src="https://img.icons8.com/metro/26/000000/upload.png" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export default MessageForm;
