44757edb6945df1e6b09ab20668c9dd60a24ad1a
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-04-05 17:18:51 +0200

    change

--- slack-chat-app/src/components/Messages/messagesHeader.js
+++ slack-chat-app/src/components/Messages/messagesHeader.js
@@ -1,11 +1,14 @@
 const MessagesHeader = () => {
     return ( <div className="messageHeader">
         <div className="header">
-            <h1>Channel<span>slika</span></h1>
+            <h1>Channel<span><img src="https://img.icons8.com/metro/26/000000/star.png"/></span></h1>
             <h2>2 Users</h2>
         </div>
         <div className="searchMessage">
-            <input type="search" name='search' placeholder='search'/>
+            <div className='inputBox'>
+
+            <input type="search" name='search' placeholder='Search Messages'/><span><label htmlFor="search"><img style={{backgroundColor:'lightcyan'}} src="https://img.icons8.com/android/24/000000/search.png"/></label></span>
+            </div>
         </div>
     </div> );
 }
