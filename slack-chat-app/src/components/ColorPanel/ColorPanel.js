1af82b3ac7189c22b29c83518a0d3846a57ec6b7
StevanZivadinovic <stevanzivadinovic@outlook.com>
2021-05-02 22:36:18 +0200

    change

--- slack-chat-app/src/components/ColorPanel/ColorPanel.js
+++ slack-chat-app/src/components/ColorPanel/ColorPanel.js
@@ -1,9 +1,31 @@
-import plus from './../../img/plus.svg'
+import plus from './../../img/plus.svg';
+import {useState} from 'react';
 
 const ColorPanel = () => {
+    
+    let showPicker = ()=>{
+        
+            document.querySelector('.picker').style.display='block';
+           
+    }
+    let removeColorPicker = ()=>{
+        document.querySelector('.picker').style.display='none';
+    }
+    
     return ( <div className='colorPanel'>
-       <img  src={plus}/>
-    </div> );
+       <img src={plus} onClick={showPicker} />
+       <div className='picker' style={{display:'none'}} >
+
+      
+       <input   type="color" id="favcolor" name="favcolor"></input>
+      <div>
+
+       <button value='Submit'>Submit</button>
+      <button onClick={removeColorPicker} value='Cancel'>Cancel</button>
+      </div>
+       </div>
+  
+   </div>);
 }
  
 export default ColorPanel;
